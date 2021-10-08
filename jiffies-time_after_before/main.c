4f05bf369982cb29b3a929e754947f43acf2bafe
TheSkyHere <747766860@qq.com>
2021-10-08 21:51:02 +0800

    Update20211008

---
+++ jiffies-time_after_before/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include  <linux/sockios.h>
+#include  <linux/ethtool.h>
+#include <linux/jiffies.h>
+
+
+int main(int arg, char **args)
+{
+    printf("jiffies = %d",jiffies);
+
+}
+
+     *  time_after：
+     *  time_after(a,b) returns true if the time a is after time b.
+　　同时根据 #define time_before(a,b)    time_after(b,a) ，我们可以知道  
+
+     * time_before(a,b) returns true if the time b is after time a.
