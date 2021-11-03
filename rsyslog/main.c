1de3379d271e010dc8c242ade5d0aa1847b89ecb
MaTao/Morton <matao5@huaqin.com>
2021-11-03 20:17:57 +0800

    update

---
+++ rsyslog/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+ 
+int main()
+{
+ 
+    openlog("msg-lyh", LOG_PID, LOG_LOCAL0);
+    syslog(LOG_INFO, "this is a test");
+ 
+ 
+    closelog();
+ 
+    return 0;
+
+}
